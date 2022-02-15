0777107715c1943809e4c606bfb52ce8b121eb43
Erick Terrones <erickter1815@gmail.com>
2022-02-15 00:20:31 -0500

    path restaurantes

--- api/urls.py
+++ api/urls.py
@@ -5,8 +5,7 @@ from .views import restaurantesview, usuariosview
 urlpatterns = [
 
     path('usuarios/', usuariosview.as_view(), name='lista_usuarios'),
-    path('usuarios/<int:id>', usuariosview.as_view(), name='usuarios_actions')
-    
-    #path('restaurantes/', restaurantesview.as_view(), name='lista_restaurantes'),
-   # path('restaurantes/<int:id>', restaurantesview.as_view(), name='restaurantes_actions')
+    path('usuarios/<int:id>', usuariosview.as_view(), name='usuarios_actions'),
+    path('restaurantes/', restaurantesview.as_view(), name='lista_restaurantes'),
+    path('restaurantes/<int:id>', restaurantesview.as_view(), name='restaurantes_actions')
             ]
